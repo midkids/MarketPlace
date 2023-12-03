11ffc32a1bdf1c8c03197ea5d9e251ba422e4f55
Myron Snelson <midkidscodinglab@gmail.com>
2023-12-03 14:33:27 -0500

    Added Product screen using hard-coded data, added a Splash screen, added app icons

---
+++ MarketPlace/Product.swift
@@ -0,0 +1,27 @@
+//
+//  Product.swift
+//  MarketPlace
+//
+//  Created by Myron Snelson on 12/2/23.
+//
+
+import Foundation
+import UIKit
+
+struct Product {
+    var productImage: UIImage
+    var productName: String
+    var productPrice: Int
+    var productDescription: String
+    var productSeller: Seller
+}
+
+struct Seller {
+    var sellerName: String
+}
+
+var products: [Product] = [
+    Product(productImage: UIImage.self(named: "marketplace.jpg")!, productName: "product1", productPrice: 50, productDescription: "This is just a first test description for a test product", productSeller: Seller(sellerName: "John Smith")),
+    Product(productImage: UIImage.self(named: "marketplace.jpg")!, productName: "product1", productPrice: 50, productDescription: "This is just a second test description for a test product", productSeller: Seller(sellerName: "John Smith")),
+    Product(productImage: UIImage.self(named: "marketplace.jpg")!, productName: "product1", productPrice: 50, productDescription: "This is just a third test description for a test product", productSeller: Seller(sellerName: "John Smith"))
+]
--- MarketPlace/ProductViewController.swift
+++ MarketPlace/ProductViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 class ProductViewController: UIViewController {
 
+    @IBOutlet var productImageImage: UIImageView!
+    @IBOutlet var productNameLabel: UILabel!
+    @IBOutlet var productPriceLabel: UILabel!
+    @IBOutlet var productDescriptionLabel: UILabel!
+    @IBOutlet var sellerNameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    func updateUI() {
+        let currentProduct = products[0]
+        let currentSeller = currentProduct.productSeller
+        productImageImage.image = currentProduct.productImage
+        productNameLabel.text = currentProduct.productName
+        productPriceLabel.text = "$" + String(currentProduct.productPrice)
+        productDescriptionLabel.text = currentProduct.productDescription
+        sellerNameButton.setTitle(currentSeller.sellerName, for: .normal)
+        
     }
     
 
