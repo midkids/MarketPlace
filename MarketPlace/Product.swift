7220df07151ee5eb6a3ee05302e87b60214dc97e
Myron Snelson <midkidscodinglab@gmail.com>
2023-12-06 12:36:20 -0500

    Added productID to Product structure to enable unique identification

--- MarketPlace/Product.swift
+++ MarketPlace/Product.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 struct Product {
+    var productID: Int
     var productImage: UIImage
     var productName: String
     var productPrice: Int
@@ -21,13 +22,13 @@ struct Seller {
 }
 
 var products: [Product] = [
-    Product(productImage: UIImage(imageLiteralResourceName: "macbookpro.jpg"), productName: "MacBook Pro", productPrice: 500, productDescription: "2019 MacBook Pro. Intel I5, 8GB RAM, 256GB drive. ", productSeller: Seller(sellerName: "santaclaus349")),
-    Product(productImage: UIImage(imageLiteralResourceName: "boots.jpg"), productName: "Women's boots", productPrice: 50, productDescription: "Gently used Ugg boots, size 9.", productSeller: Seller(sellerName: "santaclaus349")),
-    Product(productImage: UIImage(imageLiteralResourceName: "jacket.jpg"), productName: "Women's jacket", productPrice: 45, productDescription: "Brand new Amazon Essential lightweight jacket. Color black, size small. Received as gift. Waited too long to send back.", productSeller: Seller(sellerName: "rudolph27")),
-    Product(productImage: UIImage(imageLiteralResourceName: "googlenesthub.jpg"), productName: "Google Nest Hub", productPrice: 60, productDescription: "Second generation. 7 inch display, chalk color. Plays videos and music. Use to control smart devices in your home.", productSeller: Seller(sellerName: "rudolph27")),
-    Product(productImage: UIImage(imageLiteralResourceName: "printer.jpg"), productName: "HP printer", productPrice: 70, productDescription: "HP Deskjet model 2755 inkjet printer and scanner. Can be set up to work wirelessly.", productSeller: Seller(sellerName: "rudolph27")),
-    Product(productImage: UIImage(imageLiteralResourceName: "guitar.jpg"), productName: "Fender acoustic guitar", productPrice: 50, productDescription: "Squier model. Natural finish front, mahogany finish back. Good starter guitar. No nicks or dents. Sold for $100 new.", productSeller: Seller(sellerName: "rudolph27")),
-    Product(productImage: UIImage(imageLiteralResourceName: "tablet.jpg"), productName: "Amazon Fire Tablet", productPrice: 35, productDescription: "8 inch HD display. 32 GB memory. Black. Two years old. Minor scratches.", productSeller: Seller(sellerName: "frosty981"))
+    Product(productID: 1, productImage: UIImage(imageLiteralResourceName: "macbookpro.jpg"), productName: "MacBook Pro", productPrice: 500, productDescription: "2019 MacBook Pro. Intel I5, 8GB RAM, 256GB drive. ", productSeller: Seller(sellerName: "santaclaus349")),
+    Product(productID: 2, productImage: UIImage(imageLiteralResourceName: "boots.jpg"), productName: "Women's boots", productPrice: 50, productDescription: "Gently used Ugg boots, size 9.", productSeller: Seller(sellerName: "santaclaus349")),
+    Product(productID: 3, productImage: UIImage(imageLiteralResourceName: "jacket.jpg"), productName: "Women's jacket", productPrice: 45, productDescription: "Brand new Amazon Essential lightweight jacket. Color black, size small. Received as gift. Waited too long to send back.", productSeller: Seller(sellerName: "rudolph27")),
+    Product(productID: 4, productImage: UIImage(imageLiteralResourceName: "googlenesthub.jpg"), productName: "Google Nest Hub", productPrice: 60, productDescription: "Second generation. 7 inch display, chalk color. Plays videos and music. Use to control smart devices in your home.", productSeller: Seller(sellerName: "rudolph27")),
+    Product(productID: 5, productImage: UIImage(imageLiteralResourceName: "printer.jpg"), productName: "HP printer", productPrice: 70, productDescription: "HP Deskjet model 2755 inkjet printer and scanner. Can be set up to work wirelessly.", productSeller: Seller(sellerName: "rudolph27")),
+    Product(productID: 6, productImage: UIImage(imageLiteralResourceName: "guitar.jpg"), productName: "Fender acoustic guitar", productPrice: 50, productDescription: "Squier model. Natural finish front, mahogany finish back. Good starter guitar. No nicks or dents. Sold for $100 new.", productSeller: Seller(sellerName: "rudolph27")),
+    Product(productID: 7, productImage: UIImage(imageLiteralResourceName: "tablet.jpg"), productName: "Amazon Fire Tablet", productPrice: 35, productDescription: "8 inch HD display. 32 GB memory. Black. Two years old. Minor scratches.", productSeller: Seller(sellerName: "frosty981"))
 ]
 
 var sellers: [Seller] = [
